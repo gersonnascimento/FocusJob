1d7fbcdee923adc30564be78604ac688a5934a7d
gersonnascimento <gerson.nascimento@locaweb.com.br>
2017-06-02 21:16:46 -0300

    Falta layout e mensagem final

---
+++ FocusJob/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FocusJob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.txtAtividade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.cmbQtd = new System.Windows.Forms.ComboBox();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(15, 55);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 0;
+            this.btnAdicionar.Text = "Adicionar atividade";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAtividade
+            // 
+            this.txtAtividade.Location = new System.Drawing.Point(67, 13);
+            this.txtAtividade.Name = "txtAtividade";
+            this.txtAtividade.Size = new System.Drawing.Size(100, 20);
+            this.txtAtividade.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Atividade:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(173, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Qtd Pomodoris:";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(129, 55);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // cmbQtd
+            // 
+            this.cmbQtd.FormattingEnabled = true;
+            this.cmbQtd.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmbQtd.Location = new System.Drawing.Point(258, 11);
+            this.cmbQtd.Name = "cmbQtd";
+            this.cmbQtd.Size = new System.Drawing.Size(54, 21);
+            this.cmbQtd.TabIndex = 6;
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Location = new System.Drawing.Point(249, 55);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(75, 23);
+            this.btnFinalizar.TabIndex = 7;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 109);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.cmbQtd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAtividade);
+            this.Controls.Add(this.btnAdicionar);
+            this.Name = "Form1";
+            this.Text = "Finalizar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox txtAtividade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.ComboBox cmbQtd;
+        private System.Windows.Forms.Button btnFinalizar;
+    }
+}
+
---
+++ FocusJob/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FocusJob
+{
+    public partial class Form1 : Form
+    {
+        int pom = 0;
+        Atividade[] vet = new Atividade[20];
+        int contagem = 0;//conta as atividades inclusas pelo usuário;
+        public void limpaTxt()
+        {
+            this.txtAtividade.Text = "";
+            this.cmbQtd.Text = "";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vet[contagem] = new Atividade(Convert.ToInt16(this.cmbQtd.Text), this.txtAtividade.Text);
+            contagem++;
+            pom += Convert.ToInt16(cmbQtd.Text);
+            limpaTxt();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFinalizar_Click(object sender, EventArgs e)
+        {
+            Painel p = new Painel();
+            p.pegaVetor(vet);
+            p.contagem = this.contagem;
+            p.pom = this.pom;
+            p.pegaAtividade();
+            p.Show();
+        }
+    }
+}
